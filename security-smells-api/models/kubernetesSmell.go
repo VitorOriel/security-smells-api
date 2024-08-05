de72ef55567b02ec770a3057937bab9640f9e927
vitororiel <vitorwixmix@gmail.com>
2024-08-04 23:50:01 -0300

    Update to kubernetesSmell file

---
+++ security-smells-api/models/kubernetesSmell.go
@@ -0,0 +1,68 @@
+package models
+
+import (
+	"fmt"
+	"security-smells-api/constants"
+
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime/schema"
+)
+
+type KubernetesSmell struct {
+	Namespace         string                   `json:"namespace" validate:"required"`
+	WorkloadKind      string                   `json:"workload_kind" validate:"required"`
+	WorkloadLabelName string                   `json:"workload_label_name" validate:"required"`
+	WorkloadPosition  int                      `json:"workload_position" validate:"required"`
+	Rule              constants.KubernetesRule `json:"rule" validate:"required"`
+	Message           string                   `json:"message" validate:"required"`
+	Suggestion        string                   `json:"suggestion" validate:"required"`
+}
+
+func NewKubernetesSmell(
+	k8sObj metav1.Object,
+	k8sKind schema.ObjectKind,
+	container *corev1.Container,
+	workloadPosition int,
+	rule constants.KubernetesRule,
+) *KubernetesSmell {
+	namespace := k8sObj.GetNamespace()
+	if namespace == "" {
+		namespace = "default"
+	}
+	kubernetesSmell := &KubernetesSmell{
+		Namespace:         namespace,
+		WorkloadKind:      k8sKind.GroupVersionKind().Kind,
+		WorkloadLabelName: k8sObj.GetName(),
+		WorkloadPosition:  workloadPosition,
+		Rule:              rule,
+	}
+	kubernetesSmell.setMessageAndSuggestionByRule(container, rule)
+	return kubernetesSmell
+}
+
+func (kubernetesSmell *KubernetesSmell) setMessageAndSuggestionByRule(container *corev1.Container, rule constants.KubernetesRule) {
+	switch rule {
+	case constants.K8S_SEC_PRIVESCALATION_UNSET:
+		kubernetesSmell.Message = fmt.Sprintf("AllowPrivilegeEscalation not set into %s your container is running with AllowPrivilegeEscalation", container.Name)
+		kubernetesSmell.Suggestion = fmt.Sprintf("Please add AllowPrivilegeEscalation into %s to avoid running with AllowPrivilegeEscalation", container.Name)
+	case constants.K8S_SEC_CAPABILITIES_UNSET:
+		kubernetesSmell.Message = fmt.Sprintf("Capabilities not set into %s your container is running without Capabilities", container.Name)
+		kubernetesSmell.Suggestion = fmt.Sprintf("Please add Capabilities into %s to avoid running without Capabilities", container.Name)
+	case constants.K8S_SEC_RUNASUSER_UNSET:
+		kubernetesSmell.Message = fmt.Sprintf("RunAsUser not set into %s your container is running with root user", container.Name)
+		kubernetesSmell.Suggestion = fmt.Sprintf("Please add RunAsUser into %s to avoid running with root user", container.Name)
+	case constants.K8S_SEC_RESREQUESTS_UNSET:
+		kubernetesSmell.Message = fmt.Sprintf("Resources.Requests not set into %s your container is running without Resources.Requests", container.Name)
+		kubernetesSmell.Suggestion = fmt.Sprintf("Please add Resources.Requests into %s to avoid running without Resources.Requests", container.Name)
+	case constants.K8S_SEC_RESLIMITS_UNSET:
+		kubernetesSmell.Message = fmt.Sprintf("Resource.Limits not set into %s your container is running without Resource.Limits", container.Name)
+		kubernetesSmell.Suggestion = fmt.Sprintf("Please add Resource.Limits into %s to avoid running without Resource.Limits", container.Name)
+	case constants.K8S_SEC_ROROOTFS_UNSET:
+		kubernetesSmell.Message = fmt.Sprintf("ReadOnlyRootFilesystem not set into %s your container is running with ReadWriteRootFilesystem", container.Name)
+		kubernetesSmell.Suggestion = fmt.Sprintf("Please add ReadOnlyRootFilesystem into %s to avoid running with ReadWriteRootFilesystem", container.Name)
+	case constants.K8S_SEC_PRIVILEGED_UNSET:
+		kubernetesSmell.Message = fmt.Sprintf("Privileged not set into %s your container is running with Privileged", container.Name)
+		kubernetesSmell.Suggestion = fmt.Sprintf("Please add Privileged into %s to avoid running with Privileged", container.Name)
+	}
+}
